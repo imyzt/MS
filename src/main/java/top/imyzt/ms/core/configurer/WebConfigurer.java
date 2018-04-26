6bb664cb9901878c4b3fa2ed6c7622072686ea8a
imyzt <imyzt01@email.com>
2018-04-26 13:50:03 +0800

    建立消息转换器,将将响应数据中出现的数据转换成没有风险的数据.例如String类型的null,转换后响应数据为"",空串而非null.降低前台获取值时的风险

---
+++ src/main/java/top/imyzt/ms/core/configurer/WebConfigurer.java
@@ -0,0 +1,69 @@
+package top.imyzt.ms.core.configurer;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: imyzt
+ * @email imyzt01@gmail.com
+ * @date: 2018/4/26 13:31
+ * @description: 自定义消息转换器
+ *      将响应数据中出现的数据转换成没有风险的数据
+ *      String null --> ""
+ *      Number null --> 0
+ *      List<T> null --> []
+ *      Boolean null --> false
+ */
+@Configuration
+public class WebConfigurer extends WebMvcConfigurationSupport {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter4 converter = new FastJsonHttpMessageConverter4();
+        converter.setSupportedMediaTypes(getSupportedMediaTypes());
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(
+                //将各种类型的null转换为指定类型
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullBooleanAsFalse,
+                SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteNullListAsEmpty,
+                //禁止多重引用
+                SerializerFeature.DisableCircularReferenceDetect
+        );
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(Charset.forName("UTF-8"));
+        converters.add(converter);
+    }
+
+    private List<MediaType> getSupportedMediaTypes() {
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+        supportedMediaTypes.add(MediaType.APPLICATION_XML);
+        supportedMediaTypes.add(MediaType.IMAGE_GIF);
+        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+        supportedMediaTypes.add(MediaType.IMAGE_PNG);
+        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+        supportedMediaTypes.add(MediaType.TEXT_XML);
+        return supportedMediaTypes;
+    }
+}
