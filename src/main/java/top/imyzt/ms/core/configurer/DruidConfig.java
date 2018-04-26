a36b650be7ccd1c84edca506a3c2e1a3b4f82297
imyzt <imyzt01@email.com>
2018-04-26 12:02:27 +0800

    添加druid的配置文件 注册了一个servlet 注册了一个过滤器,使其可以支持排除和过滤

---
+++ src/main/java/top/imyzt/ms/core/configurer/DruidConfig.java
@@ -0,0 +1,49 @@
+package top.imyzt.ms.core.configurer;
+
+import cn.hutool.core.lang.Console;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: imyzt
+ * @email imyzt01@gmail.com
+ * @date: 2018/4/26 11:55
+ * @description: druid的配置信息
+ */
+@Configuration
+public class DruidConfig {
+    @Bean
+    public ServletRegistrationBean DruidStatViewServlet(){
+        Console.log("servletRegistrationBean configure start.");
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        //白名单
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+        //黑名单,如果与白名单冲突,优先白名单配置
+        servletRegistrationBean.addInitParameter("deny", "");
+        servletRegistrationBean.addInitParameter("loginUsername","admin");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        //是否可以重置
+        servletRegistrationBean.addInitParameter("resetEnable","fase");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter(){
+        Console.log("filterRegistrationBean configure start.");
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
