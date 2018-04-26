bad1805d7498d1d852468158829b051c0b321b8e
imyzt <imyzt01@email.com>
2018-04-26 11:08:56 +0800

    Mybatis-plus配置文件

---
+++ src/main/java/top/imyzt/ms/core/configurer/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package top.imyzt.ms.core.configurer;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: imyzt
+ * @email imyzt01@gmail.com
+ * @date: 2018/4/26 09:41
+ * @description: mybatis-plus配置文件
+ */
+@Configuration                  //标识此类为配置文件类
+@MapperScan("top.imyzt.ms.dao") //mybatis-plus扫描Mapper.java
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件,自动识别数据库类型
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+}
