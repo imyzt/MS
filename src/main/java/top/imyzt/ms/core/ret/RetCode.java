a11db0942e277655a2fbc4bdbbec6630afbf262d
imyzt <imyzt01@email.com>
2018-04-26 14:36:12 +0800

    将枚举类增加中文提示信息

--- src/main/java/top/imyzt/ms/core/ret/RetCode.java
+++ src/main/java/top/imyzt/ms/core/ret/RetCode.java
@@ -12,31 +12,49 @@ public enum RetCode {
     /**
      * 成功
      */
-    SUCCESS(200),
+    SUCCESS(200,"成功"),
 
     /**
      * 未找到
      */
-    NOT_FOUND(404),
+    NOT_FOUND(404,"未找到"),
 
     /**
      * 未认证
      */
-    UNAUTHORIZED(401),
+    UNAUTHORIZED(401,"未认证"),
 
     /**
      * 失败
      */
-    FALN(400),
+    FALN(400,"失败"),
 
     /**
      * 服务器内部错误
      */
-    SERVER_ERROR(500);
+    SERVER_ERROR(500,"服务器内部错误");
 
-    public int code;
+    private int code;
+    private String msg;
 
-    RetCode(int code){
+    RetCode(int code, String msg){
         this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
     }
 }
