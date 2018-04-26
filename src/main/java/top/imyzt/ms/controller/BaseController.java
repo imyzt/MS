f0cb4484b257f19180f8a27759f9937423f97f71
imyzt <imyzt01@email.com>
2018-04-26 11:08:37 +0800

    控制层基类

---
+++ src/main/java/top/imyzt/ms/controller/BaseController.java
@@ -0,0 +1,13 @@
+package top.imyzt.ms.controller;
+
+/**
+ * @author: imyzt
+ * @email imyzt01@gmail.com
+ * @date: 2018/4/26 11:00
+ * @description: 控制层的基类
+ */
+public abstract class BaseController {
+
+
+
+}
