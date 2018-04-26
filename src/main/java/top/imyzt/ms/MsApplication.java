3775ed61843a9b8e0a23ac70ac01dc789484ec20
imyzt <imyzt01@email.com>
2018-04-26 09:16:31 +0800

    初始化项目

---
+++ src/main/java/top/imyzt/ms/MsApplication.java
@@ -0,0 +1,17 @@
+package top.imyzt.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author imyzt
+ * @date 2018-4-26
+ * @deprecated SpringBoot + Mybatis Plus搭建后台管理系统
+ */
+@SpringBootApplication
+public class MsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApplication.class, args);
+	}
+}
