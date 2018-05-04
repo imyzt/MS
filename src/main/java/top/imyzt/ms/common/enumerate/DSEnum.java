e82fad2f00e1be6fe4d9dc6ec7e13aa401f438b0
imyzt <imyzt01@email.com>
2018-05-04 15:43:28 +0800

    数据源枚举

--- src/main/java/top/imyzt/ms/common/enumerate/DSEnum.java
+++ src/main/java/top/imyzt/ms/common/enumerate/DSEnum.java
@@ -1,10 +1,29 @@
-package top.imyzt.ms.core.mutidatasoucre.config;
+package top.imyzt.ms.common.enumerate;
 
 /**
+ * <p>
+ *     多数据源的枚举类
+ * </p>
+ *
  * @author: imyzt
  * @email imyzt01@gmail.com
  * @date: 2018/5/4 11:08
- * @description:
  */
-public enum DSEnum {
+public interface DSEnum {
+
+    /**
+     * 默认数据源
+     */
+    String DATA_SOURCE_MS = "datasourceMs";
+
+    /**
+     * 测试第一个数据源
+     */
+    String DATA_SOURCE_ONE = "datasourceOne";
+
+    /**
+     * 测试第二个数据源
+     */
+    String DATA_SOURCE_TWO = "datasourceTwo";
+
 }
