5d51111f0d26caa8b5caf340b25df672c7857d3f
imyzt <imyzt01@email.com>
2018-05-04 14:14:57 +0800

    扩展自AbstractRoutingDataSource类,自定义了数据源的路由,可在项目运行时通过提供的@DS注解指定需要操作的数据库.具体对应关系是通过dynamicDataSource.setTargetDataSources(Map)对应key值的数据源相对应

--- src/main/java/top/imyzt/ms/core/mutidatasoucre/config/DynamicDataSource.java
+++ src/main/java/top/imyzt/ms/core/mutidatasoucre/config/DynamicDataSource.java
@@ -1,10 +1,21 @@
 package top.imyzt.ms.core.mutidatasoucre.config;
 
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
 /**
+ *
+ * <p>
+ *  动态数据源
+ * </p>
+ *
  * @author: imyzt
  * @email imyzt01@gmail.com
  * @date: 2018/5/4 11:50
- * @description:
  */
-public class DynamicDataSource {
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DataSourceContextHolder.getDataSourceType();
+    }
 }
