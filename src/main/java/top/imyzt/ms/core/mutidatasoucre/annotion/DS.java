49e0812774b998a331eaafcf3ba776a17add2fd5
imyzt <imyzt01@email.com>
2018-05-04 14:09:52 +0800

    多数据源时标识数据源的注解

--- src/main/java/top/imyzt/ms/core/mutidatasoucre/annotion/DS.java
+++ src/main/java/top/imyzt/ms/core/mutidatasoucre/annotion/DS.java
@@ -1,10 +1,22 @@
 package top.imyzt.ms.core.mutidatasoucre.annotion;
 
+import java.lang.annotation.*;
+
 /**
+ *
+ * <p>
+ *     多数据源标识
+ * </p>
+ *
  * @author: imyzt
  * @email imyzt01@gmail.com
  * @date: 2018/5/4 10:10
- * @description:
  */
-public interface DS {
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface DS {
+
+    String dbname() default "";
 }
