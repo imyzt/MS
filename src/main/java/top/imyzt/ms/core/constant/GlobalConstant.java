5fc31500469e9efa49a5adf18416b73706231037
imyzt <imyzt01@email.com>
2018-05-30 11:07:38 +0800

    系统全局常量

---
+++ src/main/java/top/imyzt/ms/core/constant/GlobalConstant.java
@@ -0,0 +1,20 @@
+package top.imyzt.ms.core.constant;
+
+/**
+ * <p>
+ * 全局常量
+ * </p>
+ *
+ * @author: imyzt
+ * @email imyzt01@gmail.com
+ * @date: 2018/5/12 11:46
+ */
+public interface GlobalConstant {
+
+    /**
+     * 上传文件存放路径
+     */
+    String UPLOAD_FILE_SAVE_PATH = "D:/y/upload";
+
+
+}
