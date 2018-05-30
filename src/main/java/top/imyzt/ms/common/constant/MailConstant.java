caaea95895cecf49909f3338a5c5545b502d0dc8
imyzt <imyzt01@email.com>
2018-05-30 17:18:42 +0800

    添加了邮件常量

---
+++ src/main/java/top/imyzt/ms/common/constant/MailConstant.java
@@ -0,0 +1,24 @@
+package top.imyzt.ms.common.constant;
+
+/**
+ * <p>
+ * 邮件常量
+ * </p>
+ *
+ * @author: imyzt
+ * @email imyzt01@gmail.com
+ * @date: 2018/5/12 11:46
+ */
+public interface MailConstant {
+
+    /**
+     * <注册> 的模板名称
+     */
+    String RETGISTER_TEMPLATE = "register.ftl";
+
+    /**
+     * 模板存放路径
+     */
+    String TEMPLATE_PATH = "src/main/resources/templates/mail";
+
+}
